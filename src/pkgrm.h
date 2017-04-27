db5be7adde2735e6c9d05116f3645439be492ccf
tnut <tnut at nutyx dot org>
2017-04-27 11:12:07 +0200

    pkgrm, cleanup

--- src/pkgrm.h
+++ src/pkgrm.h
@@ -1,9 +1,9 @@
 //
-//  cards
+//  pkgrm.h
 // 
 //  Copyright (c) 2000-2005 Per Liden
 //  Copyright (c) 2006-2013 by CRUX team (http://crux.nu)
-//  Copyright (c) 2013-2016 by NuTyX team (http://nutyx.org)
+//  Copyright (c) 2013-2017 by NuTyX team (http://nutyx.org)
 // 
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
