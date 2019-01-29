1d5b6224690d17d4c3394ee19d50b918cb6f06e9
Artemia <artemia@nutyx.org>
2019-01-29 19:20:22 +0100

    Add Theme change feature by creating new -th or --theme command

--- fltk/flcards.cxx
+++ fltk/flcards.cxx
@@ -33,7 +33,7 @@ using namespace cards;
 
 int helpFlag = 0;
 bool isInstaller = false;
-
+string Theme = "gtk+";
 
 ///
 /// Argument parser for FlCards application in Fltk context
@@ -52,6 +52,13 @@ int arg_parser( int argc, char** argv, int &i )
         i += 1;
         return 1;
     }
+    if (strcmp("-th", argv[i]) == 0 || strcmp("--theme", argv[i]) == 0)
+    {
+        if (argv[i+1]==nullptr) return 0;
+        Theme = argv[i+1];
+        i += 2;
+        return 1;
+    }
     return 0;
 }
 
@@ -60,8 +67,6 @@ int arg_parser( int argc, char** argv, int &i )
 ///
 int main(int argc, char **argv)
 {
-    Fl::scheme("gtk+");
-    // Enable Multithreading on Fltk
     Fl::lock();
     int i = 1;
     if (Fl::args(argc, argv, i, arg_parser) < argc)
@@ -70,16 +75,30 @@ int main(int argc, char **argv)
               "usage: %s [options]\n"
               " -h | --help             : print extended help message\n"
               " -ni | --nutyx-installer : launch FlCards in Installer mode\n"
+              " -th | --theme           : choose Fltk theme.\n"
+              "                           Valid entries are :\n"
+              "                           none or base - default look-n-feel\n"
+              "                           plastic - inspired by Aqua user interface\n"
+              "                           gtk+ - inspired by Red Hat Bluecurve theme\n"
+              "                           gleam - inspired by Clearlooks Glossy scheme\n"
               " plus standard fltk options\n",
               argv[i], argv[0]);
     if (helpFlag)
         Fl::fatal("usage: %s [options]\n"
               " -h | --help             : print extended help message\n"
               " -ni | --nutyx-installer : launch FlCards in Installer mode\n"
+              " -th | --theme           : choose Fltk theme.\n"
+              "                           Valid entries are :\n"
+              "                           none or base - default look-n-feel\n"
+              "                           plastic - inspired by Aqua user interface\n"
+              "                           gtk+ - inspired by Red Hat Bluecurve theme\n"
+              "                           gleam - inspired by Clearlooks Glossy scheme\n"
               " plus standard fltk options:\n"
               "%s\n", argv[0], Fl::help);
 
     //Enable Log Manager
+    if (Theme.length() > 0) Fl::scheme(Theme.c_str());
+    // Enable Multithreading on Fltk
     Fl::add_idle(CLogger::loopCallback);
     mainWindow win(isInstaller);
     win.resizable(win);
