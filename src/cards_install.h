b42d04672d6f4708c49dbf39ca91cc584535e511
tnut <tnut@nutyx.org>
2022-06-19 21:06:27 +0200

    cards install update year

--- src/cards_install.h
+++ src/cards_install.h
@@ -1,7 +1,7 @@
 /*
  * cards_install.h
  * 
- * Copyright 2015 - 2020 Thierry Nuttens <tnut@nutyx.org>
+ * Copyright 2015 - 2022 Thierry Nuttens <tnut@nutyx.org>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
