dddfc91c4972d7ebc883a56037e46ba2174d0489
tnut <tnut@nutyx.org>
2023-07-22 21:03:58 +0200

    test.cpp some more tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,20 @@
 #include "../src/repodwl.h"
+#include "../src/pkgsync.h"
+
 int main () {
 	Repodwl* repodwl = new Repodwl("/etc/cards.conf");
+	Pkgsync* pkgsync = new Pkgsync;
+	pkgsync->run();
 //	std::cout << "Run parseCollectionDirectory" << std::endl;
 //	repodwl.parseConfig("/etc/cards.conf");
 //	repodwl->parseCollectionDirectory();
 	std::cout << "Run checkBinaryExist" << std::endl;
-	if (repodwl->checkBinaryExist("cards"))
-			std::cout << "cards exist" << std::endl;
-	std::cout << repodwl->getPackageFileName("cards") << std::endl;
-	std::cout << repodwl->getPackageFileNameSignature("cards") << std::endl;
+//	if (!repodwl->checkBinaryExist("konsole"))
+	repodwl->downloadPackageFileName("konsole");
+	if (repodwl->checkBinaryExist("konsole"))
+			std::cout << "konsole exist" << std::endl;
+	std::cout << repodwl->getPackageFileName("konsole") << std::endl;
+	std::cout << repodwl->getPackageFileNameSignature("konsole") << std::endl;
 	delete repodwl;
 	return 0;
 }
