2da8c8d24f7847cd6efe5605be4ab12b63d2743f
tnut <tnut@nutyx.org>
2024-04-23 04:26:32 +0200

    file_utils making a template with some specific vector method in the target class makes not much sense

--- src/file_utils.h
+++ src/file_utils.h
@@ -92,26 +92,6 @@ int parseFile(std::vector<std::string>& fileContent, const char* fileName);
 int parseFile(std::string& Depends, const char* key, const char* fileName);
 bool findMD5sum(const std::string& fileName, unsigned char* result);
 bool checkMD5sum(const char * fileName, const char * MD5Sum);
-/* read a file
-   return it into a template container */
-template <class T>
-int parseFile( T& target, const char* fileName)
-{
-	FILE *fp = fopen (fileName, "r");
-	if (!fp)
-		return -1;
-	const int length = BUFSIZ;
-	char input[length];
-	std::string line;
-	while (fgets(input, length, fp)) {
-		input[strlen(input)-1] = '\0';
-		line = input;
-		target.push_back(line);
-	}
-	fclose(fp);
-	return 0;
-}
-
 
 #endif /* FILE_UTILS_H */
 // vim:set ts=2 :
