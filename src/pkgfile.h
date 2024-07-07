918c10e016ef4e63cef5874fa726d6c178fa5ee2
tnut <tnut@nutyx.org>
2024-07-07 22:53:58 +0200

    pkgfile add dependencies stuff so we can use pkgfile class as a base for level, depend and deptree

--- src/pkgfile.h
+++ src/pkgfile.h
@@ -8,26 +8,95 @@
 
 namespace cards {
 
+struct IndexLevel {
+    // The index Name of the depencency
+    unsigned int index;
+
+    // The level of the dependency:
+    // The level of dependency:
+    // 0: No dependency
+    // 1: some deps from level 0 dependencies
+    // 2: some deps from level 1 dependencies
+    // 3: ... 2  ...
+    // etc
+    int level;
+};
+
 class port : public pkg {
+
+     // The index Name used to check
+    // if dependenciesvalid
+    unsigned int m_index;
+
+    // The level of dependency:
+    // 0: No dependency
+    // 1: some deps from level 0 dependencies
+    // 2: some deps from level 1 dependencies
+    // 3: ... 2  ...
+    // etc
+    int m_level;
+
+    // number of removed dependencies
+    // when searching them
+    unsigned int m_decrement;
+
+    //TODO findout what's it is used for
+    int m_decount;
+
+    // Dependencies made of index Name
+    // This list validate all the dependencies
+    // as they are pointing the actual name via
+    // they index
+    std::vector<IndexLevel> m_indexlevel;
+
     std::set<std::string> m_dependencies;
 
 public:
     std::set<std::string> dependencies();
     void dependencies(const std::set<std::string> dependencies);
+
+    int level() {
+        return m_level;
+    }
+    void level(int level) {
+        m_level = level;
+    }
+    int decount() {
+        return m_decount;
+    }
+    void decount(int decount) {
+        m_decount = decount;
+    }
+    unsigned int index() {
+        return m_index;
+    }
+    void index(unsigned int index) {
+        m_index = index;
+    }
+    unsigned int decrement() {
+        return m_decrement;
+    }
+    void decrement(unsigned int decrement) {
+        m_decrement = decrement;
+    }
+    std::vector<IndexLevel>indexlevel() {
+        return m_indexlevel;
+    }
+
 };
 
 typedef std::map<std::string, cards::port> ports_t;
 
+class cards_level;
+
 class pkgfile {
 
-    std::string m_configFileName;
-    cards::conf m_config;
-    ports_t m_listOfPackages;
+    friend cards_level;
 
-    /**
-     * parse the "Pkgfile" file for each Package
-     *
-     */
+    std::string  m_configFileName;
+    cards::conf  m_config;
+    ports_t      m_listOfPackages;
+
     void parsePackagePkgfileFile();
 
 public:
