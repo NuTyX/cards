7dfb6f3f08359e744417b871d6c3c9bca9bd9fd0
tnut <tnut@nutyx.org>
2024-07-07 12:55:18 +0200

    file_utils add findDir with cards::vector container

--- src/file_utils.h
+++ src/file_utils.h
@@ -4,45 +4,45 @@
 
 #pragma once
 
+#include "conf.h"
 #include "md5.h"
 #include "string_utils.h"
-#include "conf.h"
+#include "vector.h"
 
-#include <utime.h>
 #include <dirent.h>
-#include <sys/param.h>
-#include <sys/file.h>
-#include <unistd.h>
-#include <libgen.h>
 #include <err.h>
+#include <libgen.h>
+#include <sys/file.h>
+#include <sys/param.h>
+#include <unistd.h>
+#include <utime.h>
 
-#define S_CARDS_MODE  0755
+#define S_CARDS_MODE 0755
 
-#define WS_NONE         0
-#define WS_RECURSIVE    (1 << 0)
-#define WS_DEFAULT      WS_RECURSIVE
-#define WS_FOLLOWLINK   (1 << 1)        /* follow symlinks */
-#define WS_DOTFILES     (1 << 2)        /* per unix convention, .file is hidden */
-#define WS_MATCHDIRS    (1 << 3)        /* if pattern is used on dir names too */
+#define WS_NONE 0
+#define WS_RECURSIVE (1 << 0)
+#define WS_DEFAULT WS_RECURSIVE
+#define WS_FOLLOWLINK (1 << 1) /* follow symlinks */
+#define WS_DOTFILES (1 << 2) /* per unix convention, .file is hidden */
+#define WS_MATCHDIRS (1 << 3) /* if pattern is used on dir names too */
 
 enum {
-        WALK_OK = 0,
-        WALK_BADPATTERN,
-        WALK_NAMETOOLONG,
-        WALK_BADIO
+    WALK_OK = 0,
+    WALK_BADPATTERN,
+    WALK_NAMETOOLONG,
+    WALK_BADIO
 };
 
-struct InfoFile
-{
-	std::string url;
-	std::string filename;
-	std::string dirname;
-	std::string md5sum;
-	long int filetime;
-	utimbuf acmodtime;
-	FILE *stream;
+struct InfoFile {
+    std::string url;
+    std::string filename;
+    std::string dirname;
+    std::string md5sum;
+    long int filetime;
+    utimbuf acmodtime;
+    FILE* stream;
 };
-void * getDatas ( void * var, FILE * file, long offset, size_t size, size_t nmemb);
+void* getDatas(void* var, FILE* file, long offset, size_t size, size_t nmemb);
 std::string trimFileName(const std::string& filename);
 time_t getEpochModifyTimeFile(const std::string& filename);
 std::string getDateFromEpoch(const time_t& epoch);
@@ -56,14 +56,15 @@ bool checkPermissionsEqual(const std::string& file1, const std::string& file2);
 bool createRecursiveDirs(const std::string& pathname);
 void cleanupMetaFiles(const std::string& basedir);
 void removeFile(const std::string& basedir, const std::string& filename);
-int copyFile(const char *  destFile, const char *  origFile);
+int copyFile(const char* destFile, const char* origFile);
 int findDir(std::set<std::string>& filesList, const std::string& basedir);
-int findDir(itemList *filenameList, const char *path);
-int findRecursiveFile(std::set<std::string>& filenameList, const char *filename, int spec);
-int readFileStripSpace(itemList *fileContent, const char *fileName);
-int readFile(itemList *fileContent, const char *fileName);
+int findDir(cards::vector& filesList, const std::string& basedir);
+int findDir(itemList* filenameList, const char* path);
+int findRecursiveFile(std::set<std::string>& filenameList, const char* filename, int spec);
+int readFileStripSpace(itemList* fileContent, const char* fileName);
+int readFile(itemList* fileContent, const char* fileName);
 int parseFile(std::set<std::string>& fileContent, const char* fileName);
 int parseFile(std::vector<std::string>& fileContent, const char* fileName);
 int parseFile(std::string& Depends, const char* key, const char* fileName);
 bool findMD5sum(const std::string& fileName, unsigned char* result);
-bool checkMD5sum(const char * fileName, const char * MD5Sum);
+bool checkMD5sum(const char* fileName, const char* MD5Sum);
