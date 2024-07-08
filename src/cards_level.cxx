b3d6ff1619efd317f12011f28f29670530b444a0
tnut <tnut@nutyx.org>
2024-07-08 20:29:38 +0200

    cards_level somes big improvements but no yet perfect

--- src/cards_level.cxx
+++ src/cards_level.cxx
@@ -2,9 +2,9 @@
 #include "cards_level.h"
 
 namespace cards {
-	
+
 cards_level::cards_level()
-	: pkgfile("/etc/cards.conf")
+    : pkgfile("/etc/cards.conf")
 {
 }
 cards_level::~cards_level()
@@ -15,28 +15,39 @@ void cards_level::level()
     if (m_listOfPackages.size() == 0)
         parsePackagePkgfileFile();
     unsigned int level = 0;
-    static unsigned int Niveau = level;
-    generate_level(Niveau);
+    generate_level(level);
 }
 void cards_level::generate_level(unsigned int& level)
 {
     bool found = false;
-    for (auto &i : m_listOfPackages) {
+    for (auto& i : m_listOfPackages) {
+        // decount == 0 means no dependencies anymore
         if (i.second.decount() == 0) {
             found = true;
+            // at next loop we don't need to look at it
+            // decount = -1
             i.second.decount(-1);
+            // Set the package level at current level value
             i.second.level(level);
-            for (auto j : m_listOfPackages) {
+            // For all the packages
+            // that still have some deps
+            // that need to be check
+            for (auto& j : m_listOfPackages) {
+                // Still some deps that need to be check
                 if (j.second.decount() > 0) {
+                    // for all the deps of the package 'j''
                     for (auto k : j.second.indexlevel()) {
-                        if (k.index == i.second.index())
-                            i.second.decrement(i.second.decrement() + 1);
+                        // if the package 'i' is found in the deps List
+                        if (k.index == i.second.index()) {
+                            // we increment the 'decrement' variable
+                            j.second.decrement(j.second.decrement() + 1);
+                        }
                     }
                 }
             }
         }
     }
-    for (auto &i : m_listOfPackages) {
+    for (auto& i : m_listOfPackages) {
         if (i.second.decrement() > 0) {
             i.second.decount(i.second.decount() - i.second.decrement());
             i.second.decrement(0);
@@ -50,9 +61,25 @@ void cards_level::generate_level(unsigned int& level)
 void cards_level::printout()
 {
     level();
-    std::cout << m_listOfPackages.size()
-              << " found packages."
-              << std::endl;
+
+    unsigned int level = 0;
+    bool found = false;
+    while (true) {
+        for (auto i : m_listOfPackages)
+            if (i.second.level() == level) {
+                std::cout << level
+                          << ": "
+                          << i.second.collection()
+                          << "/"
+                          << i.first
+                          << std::endl;
+                found = true;
+            }
+        if (!found)
+            return;
+        ++level;
+        found = false;
+    }
 }
 
-}  // endof cards namespace
+} // endof cards namespace
