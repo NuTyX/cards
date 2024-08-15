60d96fc4a1a024a3e4947d2ad5274e73d9b0569b
tnut <tnut@nutyx.org>
2024-08-15 14:48:14 +0200

    upgrade new class for taking care of the upgrade process

---
+++ src/upgrade.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#pragma once
+
+#include "sync.h"
+#include "pkginst.h"
+#include "cards_argument_parser.h"
+
+class cards_upgrade : public pkginst {
+
+	std::set<std::string>                   m_ListOfPackagesToDelete;
+	std::set<std::pair<std::string,time_t>> m_ListOfPackages;
+	const CardsArgumentParser&              m_argParser;
+	cards::sync m_Sync;
+
+public:
+	/**
+	 *
+	 *  Constructor
+	 */
+	cards_upgrade(const CardsArgumentParser& argParser,
+		const char *configFileName);
+
+	void upgradePackages();
+	/**
+	 *
+	 * Show the packages that should be update without upgrade them
+	 */
+	void dry();
+	
+	/**
+	 *
+	 *  Number of updates availables
+	 */
+	void size();
+	
+	/**
+	 *
+	 * Are they some updates availables ?
+	 */
+	void Isuptodate();
+	int  Isdownload();
+	void upgrade();
+	void summary();
+};
