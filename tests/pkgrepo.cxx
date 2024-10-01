c42afb40b48d46970d9919124484503da300708b
tnut <tnut@nutyx.org>
2024-10-01 22:00:20 +0200

    pkgrepo new tests

--- tests/pkgrepo.cxx
+++ tests/pkgrepo.cxx
@@ -1,35 +1,31 @@
 #include "../src/libcards.h"
 
 int main (int arg, char** argv) {
+
+    std::set<std::string> listOfFiles;
+    findRecursiveFile(listOfFiles,argv[2],WS_DEFAULT);
+
+    struct stat buf;
     cards::pkgrepo pkgrepo(argv[1]);
+    std::string::size_type pos;
 
-    std::string packageFileName = pkgrepo.fileName(argv[2]);
+    for (auto i :listOfFiles) {
+        pos = i.rfind( '/' );
+        if (pos != std::string::npos)
+            std::cout << '@' << i.substr( pos + 1 ) << std::endl;
+        archive a(i);
 
-    std::cout << packageFileName << std::endl;
-    std::cout << pkgrepo.dirName(argv[2]) << std::endl;
+        pkgrepo.generateHash(i);
+        pkgrepo.generateSign();
+        a.printMeta();
+        std::cout << HASHSUM << pkgrepo.hash() << std::endl;
+        std::cout << SIGNATURE << pkgrepo.sign() << std::endl;
+        if (stat(i.c_str(), &buf) != 0)
+            std::cerr << "Cannot find size of " << i << std::endl;
+        else
+            std::cout << SIZE <<buf.st_size << std::endl;
 
-    std::string fullpathfileName = pkgrepo.dirName(argv[2])  + "/" + packageFileName;
-
-    pkgrepo.generateHash(fullpathfileName);
-
-    std::cout << "hash         : " << pkgrepo.hash()
-        << std::endl;
-
-    pkgrepo.generateSign();
-
-    std::cout << "signature    : " << pkgrepo.sign()
-        << std::endl;
-
-
-    pkgrepo.sign(argv[2]);
-    pkgrepo.hash(argv[2]);
-
-    if (pkgrepo.checkSign(argv[2]))
-        std::cout << "Signature is ok";
-    else
-        std::cout << "Bad Signature";
-
-    std::cout << std::endl;
-
+        std::cout << std::endl;
+    }
     return 0;
 }
