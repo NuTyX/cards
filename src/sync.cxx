f86a752fa59e7cd29f49e16648d291c3923127f7
tnut <tnut@nutyx.org>
2024-11-02 20:13:46 +0100

    sync class download and uncompress download meta files.

--- src/sync.cxx
+++ src/sync.cxx
@@ -3,12 +3,14 @@
 
 namespace cards {
 
-const std::string sync::DEFAULT_REPOFILE = ".REPO";
+const std::string sync::DEFAULT_PKG_REPO = ".REPO";
+const std::string sync::DEFAULT_PKG_FILES = ".FILES";
 
 sync::sync()
 	:m_config("/etc/cards.conf")
 {
-    m_repoFile = DEFAULT_REPOFILE;
+    m_pkgRepoFile = DEFAULT_PKG_REPO;
+    m_pkgFilesFile = DEFAULT_PKG_FILES;
     m_root = "/";
     m_configFile = "/etc/cards.conf";
 }
@@ -17,7 +19,8 @@ sync::sync(const std::string configFile)
 	, m_configFile(configFile)
 	, m_config(m_root + m_configFile)
 {
-    m_repoFile = DEFAULT_REPOFILE;
+    m_pkgRepoFile = DEFAULT_PKG_REPO;
+    m_pkgFilesFile = DEFAULT_PKG_FILES;
 }
 void sync::purge()
 {
@@ -49,9 +52,85 @@ void sync::run()
 			+ "/"
 			+ collection.collection
 			+ "/"
-			+ m_repoFile,
+			+ m_pkgRepoFile
+			+ ".zst",
 			collection.depot + "/" + collection.collection,
-			m_repoFile, true);
+			m_pkgRepoFile, false);
+
+		uncompress(collection.depot + "/"
+					+ collection.collection
+					+ "/"
+					+ m_pkgRepoFile);
+
+		cards::dwl files(collection.url
+			+ "/"
+			+ m_config.arch()
+			+ "/"
+			+ m_config.version()
+			+ "/"
+			+ collection.collection
+			+ "/"
+			+ m_pkgFilesFile
+			+ ".zst",
+			collection.depot + "/" + collection.collection,
+			m_pkgFilesFile, false);
+
+		uncompress(collection.depot + "/"
+					+ collection.collection
+					+ "/"
+					+ m_pkgFilesFile);
 	}
 }
+void sync::uncompress(const std::string fileName)
+{
+	std::ifstream inFile(fileName + ".zst", std::ios::binary);
+	if (!inFile) {
+	std::cerr << "Cannot open file: " << fileName << "\n";
+	return;
+	}
+
+	inFile.seekg(0, std::ios::end);
+	size_t compressSize = inFile.tellg();
+	inFile.seekg(0, std::ios::beg);
+
+	std::vector<char> compressData(compressSize);
+	inFile.read(compressData.data(), compressSize);
+	inFile.close();
+
+	// Findout memory buffer
+	size_t decompressSize = ZSTD_getFrameContentSize(compressData.data(), compressSize);
+	if (decompressSize == ZSTD_CONTENTSIZE_UNKNOWN) {
+		std::cerr << "Unknown size"
+			<< std::endl;
+		return;
+	} else if (decompressSize == ZSTD_CONTENTSIZE_ERROR) {
+		std::cerr << "Something wrong in the compress file:"
+			<< fileName
+			<< std::endl;
+		return;
+	}
+
+	// Decompress
+	std::vector<char> decompressData(decompressSize);
+	size_t result = ZSTD_decompress(decompressData.data(), decompressSize, compressData.data(), compressSize);
+
+	if (ZSTD_isError(result)) {
+	std::cerr << "Error uncompressing: "
+		<< ZSTD_getErrorName(result)
+		<< std::endl;
+	return;
+	}
+
+	// Write to file
+	std::ofstream outFile(fileName, std::ios::binary|std::ios::out);
+	if (!outFile)
+		std::cerr << "Can't open file: "
+		<< fileName
+		<< std::endl;
+
+	outFile.write(decompressData.data(),decompressData.size());
+
+	outFile.close();
+}
+
 }
--- src/sync.h
+++ src/sync.h
@@ -5,22 +5,28 @@
 #include "conf.h"
 #include "dwl.h"
 
+#include <zstd.h>
+
 namespace cards {
 class sync
 {
-	std::string m_repoFile;
+	std::string m_pkgRepoFile;
+	std::string m_pkgFilesFile;
 	std::string m_root;
 	std::string m_configFile;
 
 	cards::conf m_config;
+	void        uncompress(std::string fileName);
 
 public:
 	sync();
 	sync(const std::string configFile);
 
-	void run();
-	void purge();
-	static const std::string DEFAULT_REPOFILE;
+	void        run();
+	void        purge();
+
+	static const std::string DEFAULT_PKG_REPO;
+	static const std::string DEFAULT_PKG_FILES;
 };
 
 }
