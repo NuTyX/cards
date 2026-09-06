1d870b74759b70555f5624f3dbddc5524ffa99c8
tnut <tnut@nutyx.org>
2026-09-06 14:04:07 +0200

    state works beter when libs are open :). Error handling is welcome.

--- src/state.cxx
+++ src/state.cxx
@@ -7,11 +7,16 @@ int state(const char* fileName)
     if (!L)
         return 1;
 
+    luaL_openlibs(L);
+
     int result = luaL_dofile(L, fileName);
 
-    if (result != LUA_OK)
-        return result;
+    if (result != LUA_OK) {
+        const char *error = lua_tostring(L, -1);
+        fprintf(stderr, "Lua error: %s\n",
+                error ? error : "unknown error");
 
+    }
     lua_close(L);
 
     return result;
